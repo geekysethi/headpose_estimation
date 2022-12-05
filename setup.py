ddaa073663e7d1fb8fb3329319a745a1cb071583
Ashish Sethi <sethiaashish1995@gmail.com>
2022-12-05 20:17:13 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
-VERSION = '0.0.2'
+VERSION = '0.0.4'
 DESCRIPTION = 'Head pose estimation module'
 
 # Setting up
@@ -15,6 +15,7 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
+    url="https://github.com/geekysethi/headpose_estimation",
     install_requires=['efficientnet'],
     keywords=['python', 'image', 'face detection', 'headpose estimation', 'machine learning', 'computer vision'],
     include_package_data=True,
